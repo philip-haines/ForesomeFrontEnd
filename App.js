760bf91996aedab9c56da2ec5c86e78042e9d692
Phil Haines <phil.haines92@gmail.com>
2022-01-10 16:32:37 -0700

    seperated card into own componenet

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { Text, Image, ImageBackground, View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import UserCard from './src/components/UserCard';
+import users from './assets/data/users';
 
 const App = () => {
   return (
     <View style={styles.pageContainer}>
-      <View style={styles.card}>
-        <ImageBackground 
-        source={{uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg'}}
-        style={styles.image}
-        >
-          <View style={styles.infoContainer}>
-            <Text style={styles.name}>The Zuck</Text>
-            <Text style={styles.bio}>No need to send me your nudes, I already saw them</Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <UserCard user={users[0]}/>
     </View>
-  )
+  );
 };
+
 
 const styles = StyleSheet.create({
   pageContainer: {
@@ -25,48 +18,6 @@ const styles = StyleSheet.create({
     alignItems: 'center', 
     flex: 1
   },
-  card:{
-    width: '95%',
-    backgroundColor: 'red',
-    height: '70%',
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 5,
-    },
-    shadowOpacity: 0.36,
-    shadowRadius: 6.68,
-
-    elevation: 11,
-  },
-  image: {
-    width: '100%',
-    height: undefined,
-    aspectRatio: 2.75/4.5,
-    // height: '100%',
-    borderRadius: 10,
-    overflow: 'hidden',
-    justifyContent: 'flex-end',
-    alignItems: 'flex-start',
-  },
-  infoContainer:{
-    width: '90%',
-    justifyContent: 'center',
-    padding: 10,
-    alignItems: 'flex-start',
-  },
-  name:{
-    fontSize: 30,
-    color: 'white',
-    fontWeight: 'bold'
-  },
-  bio:{
-    fontSize: 16,
-    color: 'white',
-    lineHeight: 24,
-  },
 });
-
 
 export default App;
---
+++ src/components/UserCard/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Text, View, ImageBackground, StyleSheet } from 'react-native'
+
+const UserCard = (props) => {
+  const {name, image, bio} = props.user
+  return (
+    <View style={styles.card}>
+      <ImageBackground 
+      source={{uri: image}}
+      style={styles.image}
+      >
+        <View style={styles.infoContainer}>
+          <Text style={styles.name}>{ name }</Text>
+          <Text style={styles.bio}>{ bio }</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  pageContainer: {
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    flex: 1
+  },
+  card:{
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.36,
+    shadowRadius: 6.68,
+
+    elevation: 11,
+  },
+  image: {
+    width: '100%',
+    height: undefined,
+    aspectRatio: 2.75/4.5,
+    // height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start',
+  },
+  infoContainer:{
+    width: '90%',
+    justifyContent: 'center',
+    padding: 10,
+    alignItems: 'flex-start',
+  },
+  name:{
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  bio:{
+    fontSize: 16,
+    color: 'white',
+    lineHeight: 24,
+  },
+});
+
+export default UserCard;
